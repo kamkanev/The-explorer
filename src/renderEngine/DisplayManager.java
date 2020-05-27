29f5717a0159ffe8cb420a626786b5e3d9185265
kamkanev <kamkanev@gmail.com>
2020-05-27 12:22:37 +0300

    update title

--- src/renderEngine/DisplayManager.java
+++ src/renderEngine/DisplayManager.java
@@ -25,7 +25,7 @@ public class DisplayManager {
 		try {
 			Display.setDisplayMode(new DisplayMode(WIDTH,HEIGHT));
 			Display.create(new PixelFormat(), attribs);
-			Display.setTitle("My First Game engine!");
+			Display.setTitle("The Explorer");
 		} catch (LWJGLException e) {
 			e.printStackTrace();
 		}
